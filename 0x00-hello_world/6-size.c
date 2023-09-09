6ab4195c449a50f396b1059c47c797409f907107
Valeryio <tommagoma23@gmail.com>
2023-09-09 09:57:25 -0700

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,16 @@
 /**
  * main - show the size of elements
  *
- * Description: Display the  size of different var
- * Return 0;
+ * Description: Display the  size of
+ * different var.
+ *
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
