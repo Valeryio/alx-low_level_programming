067708c160c9e812391904f78dc8141c843b4f69
Valeryio <tommagoma23@gmail.com>
2023-09-18 02:32:49 -0700

    Updating the first project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - updating value
+ * @n: a pointer
+ *
+ * Description: This function takes modify some values.
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
