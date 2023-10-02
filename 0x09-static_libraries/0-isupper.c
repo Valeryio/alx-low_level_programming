6e1c52ac0fd25977304f6b34265b10105d6c974b
Valeryio <tommagoma23@gmail.com>
2023-10-02 08:02:29 -0700

    Update

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * _isupper - Check if paramter c represents an uppercase letter.
- *   * @c: Int representing a character
- *    * Return: Always 0.
- *     */
+ ** _isupper - Check if paramter c represents an uppercase letter.
+ ** @c: Int representing a character
+ ** Return: Always 0.
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -24,3 +24,8 @@ You are not allowed to use static variables
 ## Student
 **DMT Linson**
 
+## Task 0
+Create the static library libmy.a containing all the functions listed in the project.
+
+## Task 1
+Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.
