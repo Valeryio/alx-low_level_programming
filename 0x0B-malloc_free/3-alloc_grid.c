877bee3db5774a13fe0d38e1e7bbc8e8959bd05f
Valeryio <tommagoma23@gmail.com>
2023-10-04 13:47:37 -0700

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -43,10 +43,13 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
-	
-	for (i = 0; i < height; i++)	
+	if (grid[i] == NULL || grid == NULL)
 	{
-		free(grid[i]);
+		for (i = 0; i < height; i++)	
+		{
+			free(grid[i]);
+		}
+		free(grid);
 	}
 
 	return (grid);
