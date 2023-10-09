10b6300bb7b37fa4970cc6534f40d2b6ed99332b
Valeryio <tommagoma23@gmail.com>
2023-10-09 07:33:22 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,11 +30,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 
 
-	for (k = 0; k < lens2; k++)
+	for (k = 0; s2 != NULL && k < lens2; k++)
 	{
 		ptr[i] = s2[k];
 		i++;
