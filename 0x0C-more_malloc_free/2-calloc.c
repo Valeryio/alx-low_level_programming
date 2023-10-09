c5da8627f4ba6ba163afdd658f2187174633161a
Valeryio <tommagoma23@gmail.com>
2023-10-09 07:57:04 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = NULL;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,9 +22,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-/*
+
 	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
-*/
+
 	return (ptr);
 }
