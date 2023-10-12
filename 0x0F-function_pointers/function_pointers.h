d3843b1e9b82c4b2cff1f5ed10b0e40c5e88516a
Valeryio <tommagoma23@gmail.com>
2023-10-12 06:30:22 -0700

    update-N

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef __MHEADER__
+#define __MHEADER__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,3 +9,5 @@ char _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
