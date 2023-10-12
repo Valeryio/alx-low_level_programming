9090cb28f8f36c985c9e7a9f0a9c61d22f762502
Valeryio <tommagoma23@gmail.com>
2023-10-12 06:28:18 -0700

    update-N

--- 0x0F-function_pointers/okay.c
+++ 0x0F-function_pointers/okay.c
@@ -1,6 +1,24 @@
 #include "function_pointers.h"
 
 /**
+ * printer - print a string
+ * @s: (string), the string to print
+ *
+ * Description: This function print a
+ * string
+ */
+
+void printer(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+}
+
+
+
+/**
  * print_name - print a name
  * @name: (string), the name to print
  * @f: (pointer to function)
