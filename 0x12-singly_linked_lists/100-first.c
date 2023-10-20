87df72e93f71ce37e2fe7131732af909d5b9461c
Valeryio <tommagoma23@gmail.com>
2023-10-19 19:47:42 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+
+int main(void)
+{
+	func();
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
