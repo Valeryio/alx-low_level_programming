6741bc7a9cbb06c2766b1b517ea5655aa0c6b38b
Valeryio <tommagoma23@gmail.com>
2023-10-19 19:54:40 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,15 +6,7 @@
  * Return: Always 0.
  */
 
-void func(void)
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-
-int main(void)
-{
-	func();
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
 }
