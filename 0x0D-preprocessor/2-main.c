54c92f62317c0c5f7c8b3dc0f1541b09dce85460
Valeryio <tommagoma23@gmail.com>
2023-10-24 05:42:52 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,12 @@
 /**
  * main - print a file name
  *
- * Descripion: prints a file's name
+ * Description: prints a file's name
+ * Return: An integer
  */
 
 int main(void)
 {
 	printf("%s\n", __BASE_FILE__);
-	return(0);
+	return (0);
 }
