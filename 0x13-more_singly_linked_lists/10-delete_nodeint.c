da570d76c68412db59802d15b90f87fbdfa27c41
Valeryio <tommagoma23@gmail.com>
2023-10-24 13:41:54 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	tmp = *head;
 
-	for (i = 0; i < (int) (index - 1) && tmp; i++)
+	for (i = 0; i < (int) (index - 1); i++)
 	{
 		tmp = tmp->next;
 	}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (p);
 	}
 
-	for (i = 1; i < (int) idx; i++)
+	for (i = 0; i < (int) idx; i++)
 	{
 		if(!tmp)
 			return (NULL);
