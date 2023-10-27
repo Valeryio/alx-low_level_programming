5683da2bbdeb473f91c0ef97262a9d382ca9b5ff
Valeryio <tommagoma23@gmail.com>
2023-10-27 07:44:27 -0700

    update

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -8,13 +8,16 @@
  *     */
 int main(void)
 {
-	    int n;
+	    unsigned long int n;
 
-	        n = get_bit(1, 10);
-		    printf("%d\n", n);
-		        n = get_bit(98, 1);
-			    printf("%d\n", n);
-			        n = get_bit(1024, 0);
-				    printf("%d\n", n);
-				        return (0);
+	        n = 1024;
+		    clear_bit(&n, 10);
+		        printf("%lu\n", n);
+			    n = 0;
+			        clear_bit(&n, 10);
+				    printf("%lu\n", n);
+				        n = 98;
+					    clear_bit(&n, 1);
+					        printf("%lu\n", n);
+						    return (0);
 }
