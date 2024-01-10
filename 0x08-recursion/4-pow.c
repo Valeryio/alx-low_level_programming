eb5bc00e881a6f47083f5e9be97be8207c24dd47
Valeryio <tommagoma23@gmail.com>
2024-01-10 12:30:52 -0800

    Updating the task 4

---
+++ 0x08-recursion/4-pow.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the pow
+ * @x: (int)
+ * @y: (int)
+ *
+ * Description: This function calculate
+ * the pow of a number.
+ * Return: an integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+
+	if (y == 0)
+		return (1);
+
+	if (y < 0)
+		return (-1);
+
+	return (x * _pow_recursion(x * x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,4 @@ void _puts_recursion(char*);
 void _print_rev_recursion(char*);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int, int);
