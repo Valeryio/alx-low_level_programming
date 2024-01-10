08695c3d24a2d89729018f3a0661debad4a89ce4
Valeryio <tommagoma23@gmail.com>
2024-01-10 03:39:34 -0800

    Updating the task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - print characters
  * @s: (char)
- * 
+ *`
  * Description: This function prints all
  * the characters of a sent string
  */
@@ -11,7 +11,7 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{	
+	{
 		_putchar('\n');
 		return;
 	}
