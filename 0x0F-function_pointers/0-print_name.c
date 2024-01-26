114f5c211958870e1bbb64e0ecdaf4730fa41ca1
Valeryio <tommagoma23@gmail.com>
2024-01-26 02:02:21 -0800

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_name - print a name
+ * @name: (string)
+ * @f: (function to pointer)
+ *
+ * Description: This function prints
+ * the name of people
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return (NULL);
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+char _putchar(char c);
+void print_name(char*, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
