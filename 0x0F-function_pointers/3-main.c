66ad5a10c2d023a77507f423c881b8f4dd63eff3
Valeryio <tommagoma23@gmail.com>
2024-03-06 10:00:09 +0000

    Updating of some errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,20 +21,26 @@ int main(int argc, char *argv[])
 	*/
 	if (argc < 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 
 	/* Verifying if the operator is in the list*/
-	if ((argv[2][0] != '+') && (argv[2][0] != '-') && (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%'))
+	if (((argv[2][0] != '+') && (argv[2][0] != '-') && (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%')) || (strlen(argv[2]) > 1))
+	{
+		printf("Error\n");
 		exit(99);
-	
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 
 	if ((b == 0) && ((argv[2][0] == '/') || (argv[2][0] == '%')))
+	{
+		printf("Error\n");
 		exit(100);
+	}
 
 	result = get_op_func(argv[2])(a, b);
 
