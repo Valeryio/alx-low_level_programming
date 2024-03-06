5655f5a4009d12a6da50bb1354005e291a4cecda
Valeryio <tommagoma23@gmail.com>
2024-03-05 20:29:32 -0800

    Update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -19,9 +19,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 	}
 
-	if (!name || !owner)
-		exit(EXIT_FAILURE);
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
