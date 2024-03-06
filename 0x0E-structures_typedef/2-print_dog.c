6ad266755da0fcd013a7c80db0f9c69d618bbecd
Valeryio <tommagoma23@gmail.com>
2024-03-05 20:25:46 -0800

    Update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (!d)
 		exit(EXIT_FAILURE);
 
-	(!d->name) ? printf("nil\n") : printf("Name: %s\n", d->name);
+	(!d->name) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	(!d->owner) ? printf("nil\n") : printf("Owner: %s\n", d->owner);
+	(!d->owner) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
