426578deb629908d9aebf2ac56c03ce3cb038341
Valeryio <tommagoma23@gmail.com>
2024-03-05 19:54:12 -0800

    Updating betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,7 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
 #include <stdio.h>
 
 /**
@@ -6,14 +10,18 @@
  * @owner: (char*)  the name of the owner of the dog
  * @age: (float) the age of the dog
  *
- * Description: That is an object that 
+ * Description: That is an object that
  * defines a dog.
  */
 
-typedef struct dog {
+typedef struct dog
+{
 	char *name;
 	char *owner;
 	float age;
 } dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+#endif
