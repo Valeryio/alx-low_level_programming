d08187261abd9d92bb8b6d5a5028fa8e3e1b45fa
Valeryio <tommagoma23@gmail.com>
2024-03-21 09:01:40 -0700

    UPdate

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,13 +11,16 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp_node = NULL;
-
+	dlistint_t *tmp_node = NULL, *to_delete = NULL;
 	tmp_node = head;
 
-	while(!tmp_node->next)
-	{
+	while(tmp_node)
+	{	
+		printf("Ok\n");
+		to_delete = tmp_node;
 		tmp_node = tmp_node->next;
-		free(tmp_node->prev);
+		printf("current : %d; next : %d\n", to_delete->n, tmp_node->n);
+		free(to_delete);
 	}
+	printf("End : %d\n", to_delete->n);
 }
