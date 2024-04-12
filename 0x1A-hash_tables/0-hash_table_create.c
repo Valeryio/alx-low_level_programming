e9aae01ae523fc306b0ab5956c9115e28ec8303d
Valeryio <tommagoma23@gmail.com>
2024-04-12 13:28:49 +0000

    Update the task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t* table = NULL;
+	unsigned long int i = 0;
 
 	table = (hash_table_t*) malloc(sizeof(hash_table_t));
 
@@ -19,7 +20,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = NULL;
+	table->array = (hash_node_t**) malloc(sizeof(hash_node_t*) * size);
+
+	while (i < size)
+	{
+		table->array[i] = NULL;
+		i++;
+	}
 
 	return (table);
 }
