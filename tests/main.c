8623b70691eb5afd81e3462dacbb52c0ea77a0ee
Valeryio <tommagoma23@gmail.com>
2024-04-12 12:57:09 +0000

    commit on the projecct

---
+++ tests/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int palindrome(int number);
+
+int main(void)
+{
+	palindrome(9009);
+}
+
+
+int palindrome(int number)
+{
+	char *newpalindrome = NULL;
+	int numberlen = 1;
+
+
+
+	printf("%s\n", newpalindrome);
+}
