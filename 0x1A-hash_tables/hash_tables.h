ccfd329c76e861c554fa029c353050bf26a30315
Valeryio <tommagoma23@gmail.com>
2024-04-12 13:38:38 +0000

    Updating the header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef HASH_TABLES_H_
-#define HASH_TABLES_H_
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,10 +30,10 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t* hash_table_create(unsigned long int);
+hash_table_t *hash_table_create(unsigned long int);
 
 #endif
