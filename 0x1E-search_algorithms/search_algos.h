8849dde80d1ddae00cb232dd9b48e125d269f8da
Valeryio <tommagoma23@gmail.com>
2024-07-16 04:51:18 +0000

    update the task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,11 @@
 #ifndef _SEARCH_ALGO_H
 #define _SEARCH_ALGO_H
 
-	#include <stdio.h>
-	#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	int linear_search(int *array, size_t size, int value);
-	int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 
 #endif
